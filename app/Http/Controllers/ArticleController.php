377dd5e149f6e44e70983de945d6afbcbb40923d
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-05-17 00:13:01 +0900

    finished part 4-3

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,9 +9,13 @@ class ArticleController extends Controller
 {
     public function index()
     {
-
         $articles = Article::all()->sortByDesc('created_at');
 
         return view('articles.index', ['articles' => $articles]);
+    }
+
+    public function create()
+    {
+        return view('articles.create');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,8 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
-Route::resource('/articles', ArticleController::class)->except(['index']);
+
+Route::resource('/articles', ArticleController::class)->except(['index'])->middleware('auth');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
